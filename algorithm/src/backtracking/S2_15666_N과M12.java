a53b0f58d196d0eb3afcaa0b60e3019be7236226
이은지 <esc3116@naver.com>
2024-12-11 22:58:05 +0900

    backtracking

--- algorithm/src/backtracking/S2_15666_N과M12.java
+++ algorithm/src/backtracking/S2_15666_N과M12.java
@@ -1,9 +1,60 @@
 package backtracking;
 
-public class S2_15666_N과M12 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-	public static void main(String[] args) {
-		System.out.println("은지다");
+public class S2_15666_N과M12 {
+	
+	static int N, M;
+	static int[] list, out;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+
+		list = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			list[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		out = new int[M];
+		
+		Arrays.sort(list);
+		
+		dfs(0, 0);
+		
+		System.out.println(sb.toString());
+		
+	}
+	
+	public static void dfs(int idx, int d) {
+		if(d == M) {
+			for(int i=0; i<M; i++) {
+				sb.append(out[i]).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		int before = -1;
+		for(int i=idx; i<N; i++) {
+			if(before != list[i]) {
+				before = list[i];
+				out[d] = list[i];
+				dfs(i, d+1);
+			}
+		}
 	}
 
 }
