7310e94b1950a55eb87ad2fe5804ac83b4912bed
이은지 <esc3116@naver.com>
2024-12-12 17:11:15 +0900

    분할정복

---
+++ algorithm/src/DivideAndConquer/README.md
@@ -0,0 +1,13 @@
+# 분할정복
+
+[메인으로 돌아가기](https://github.com/dmswldk28/baekjoon)
+
+다시 풀어볼 문제에 체크( :heavy_check_mark: )
+
+<br>
+
+
+[백준 문제집](https://www.acmicpc.net/workbook/view/7135)
+|          순번          |        추천 문제         |        문제 번호         |        문제 이름         |         난이도          |        풀이 링크         |
+| :-----: | :-----: | :-----: | :-----: | :-----: | :-----: |
+| 00 |  :heavy_check_mark:  | <a href="https://www.acmicpc.net/problem/1629" target="_blank">1629</a> | <a href="https://www.acmicpc.net/problem/1629" target="_blank">곱셈</a> | <img height="25px" width="25px" src="https://static.solved.ac/tier_small/10.svg"/> | <a href="./../src/backtracking/S2_15666_N과M12.java">바로가기</a> |
---
+++ algorithm/src/DivideAndConquer/S1_1629_곱셈.java
@@ -0,0 +1,34 @@
+package DivideAndConquer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S1_1629_곱셈 {
+	static int A, B, C;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		long result = 1;
+		A = A % C;
+		
+		while(B > 0) {
+			if(B%2 == 1) {
+				result = result * A % C;
+			}
+			A = (A * A) % C;
+			B = B / 2;
+		}
+		
+		System.out.println(result);
+
+	}
+
+}
